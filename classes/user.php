0026e042130f1a93dc552cf3fa2bd20135b5a312
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-09 16:05:44 +0800

    moved functions in user class (deciding what to do with viewUser)

--- classes/user.php
+++ classes/user.php
@@ -9,37 +9,6 @@ class User extends Database
 	private $middle_name = "";
 	private $username = "";
 	private $password = "";
-	public function addUser($first_name, $last_name, $middle_name = "", $username, $password)
-	{
-		$sql = "INSERT INTO user (first_name, last_name, middle_name, username, password) VALUES (:first_name, :last_name, :middle_name, :username, :password);";
-
-		$query = $this->connect()->prepare($sql);
-
-		$query->bindParam(":first_name", $first_name);
-		$query->bindParam(":last_name", $last_name);
-		$query->bindParam(":middle_name", $middle_name);
-		$query->bindParam(":username", $username);
-		$query->bindParam(":password", $password);
-
-		return $query->execute();
-	}
-
-	public function viewUser($search = "", $genre = "")
-	{
-		$sql = "SELECT * FROM employee WHERE title LIKE CONCAT('%', :search, '%') AND genre LIKE CONCAT('%', :genre, '%') ORDER BY title ASC;";
-
-		$query = $this->connect()->prepare($sql);
-
-		$query->bindParam(":search", $search);
-		$query->bindParam(":genre", $genre);
-
-		if ($query->execute()) {
-			return $query->fetchAll();
-		} else {
-			return null;
-		}
-	}
-
 	public function userExists($username)
 	{
 		$sql = "SELECT * FROM user WHERE username = :username LIMIT 1;";
@@ -58,6 +27,37 @@ class User extends Database
 
 		return $query->fetch(PDO::FETCH_ASSOC) ?: null;
 	}
+	public function addUser($first_name, $last_name, $middle_name = "", $username, $password)
+	{
+		$sql = "INSERT INTO user (first_name, last_name, middle_name, username, password) VALUES (:first_name, :last_name, :middle_name, :username, :password);";
+
+		$query = $this->connect()->prepare($sql);
+
+		$query->bindParam(":first_name", $first_name);
+		$query->bindParam(":last_name", $last_name);
+		$query->bindParam(":middle_name", $middle_name);
+		$query->bindParam(":username", $username);
+		$query->bindParam(":password", $password);
+
+		return $query->execute();
+	}
+
+	public function viewUser($search = "", $genre = "")
+	{
+		$sql = "SELECT * FROM daily_time_record WHERE title LIKE CONCAT('%', :search, '%') AND genre LIKE CONCAT('%', :genre, '%') ORDER BY title ASC;";
+
+		$query = $this->connect()->prepare($sql);
+
+		$query->bindParam(":search", $search);
+		$query->bindParam(":genre", $genre);
+
+		if ($query->execute()) {
+			return $query->fetchAll();
+		} else {
+			return null;
+		}
+	}
+
 	public function editUser($id)
 	{
 		$sql = "UPDATE user SET first_name = :first_name, last_name = :last_name, middle_name = :middle_name, username = :username, password = :password WHERE id = :id;";
