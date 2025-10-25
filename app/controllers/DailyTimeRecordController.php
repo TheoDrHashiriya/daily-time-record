8028a10d81edbf48e02628368e56fe8fe646aca5
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-25 17:26:01 +0800

    added time out controller and some minor refactor

--- app/controllers/DailyTimeRecordController.php
+++ app/controllers/DailyTimeRecordController.php
@@ -10,20 +10,33 @@ class DTRController extends DailyTimeRecord
 		$this->dtrModel = new DailyTimeRecord();
 	}
 
-	public function hasUnclosedRecord($id)
+	public function hasUnclosedRecord($userId)
 	{
 
 	}
 
 	public function timeIn($userId)
 	{
-		if ($this->dtrModel->hasTimedInToday($userId)) {
+		if ($this->dtrModel->hasTimedInToday($userId))
 			return ["error" => "You have already clocked in today."];
-		}
 
-		if ($this->dtrModel->recordTimeIn($userId)) {
+		if ($this->dtrModel->recordTimeIn($userId))
 			return ["success" => "Time-in recorded successfully."];
-		}
+
+		$_SESSION["has_timed_in_today"] = $this->dtrModel->hasTimedInToday($userId);
+
+		return ["error" => "Failed to record time-in."];
+	}
+
+	public function timeOut($userId)
+	{
+		if (!$this->dtrModel->hasTimedInToday($userId))
+			return ["error" => "You have not yet clocked in today."];
+
+		if ($this->dtrModel->recordTimeOut($userId))
+			return ["success" => "Time-out recorded successfully."];
+
+		$_SESSION["has_timed_out_today"] = $this->dtrModel->hasTimedOutToday($userId);
 
 		return ["error" => "Failed to record time-in."];
 	}
