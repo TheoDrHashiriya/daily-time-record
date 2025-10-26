fe3a42e4c84b0502a797e42a4d67544f9c81d8b4
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-26 13:00:05 +0800

    time out now disappears if already timed out

--- app/views/index.php
+++ app/views/index.php
@@ -138,7 +138,8 @@
 					<div class="card" id="time-in">
 						<a href="timein" id="time-in">Time In</a>
 					</div>
-				<?php else: ?>
+				<?php endif;
+				if (!$_SESSION["has_timed_out_today"]): ?>
 					<div class="card" id="time-out">
 						<a href="timeout" id="time-in">Time Out</a>
 					</div>
