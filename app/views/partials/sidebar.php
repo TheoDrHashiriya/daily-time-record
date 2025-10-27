b96b027cb7bfb19c8a0a7d4ff6526ba77b7ecd84
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-27 20:58:44 +0800

    revised sidebar contents

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -1,10 +1,16 @@
-<section class="sidebar">
-	<?php if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"]):
-		if ($userRole === "admin"): ?>
-			<a href="#" data-target="home-section">Home</a>
-			<a href="#" data-target="users-section">Users</a>
-			<a href="#" data-target="records-section">Records</a>
-		<?php endif;
-	endif; ?>
+<section id="sidebar" class="sidebar">
+	<?php if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"]): ?>
+		<div class="top">
+			<a class="nav" href="#" data-target="home-section">Home</a>
+			<a class="nav" href="#" data-target="records-section">Records</a>
 
+			<?php if ($userRole === "admin"): ?>
+				<a class="nav" href="#" data-target="users-section">Users</a>
+			<?php endif; ?>
+		</div>
+
+		<div class="bottom">
+			<a id="logout" href="logout">Logout</a>
+		</div>
+	<?php endif; ?>
 </section>
