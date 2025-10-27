93ce821d2a0e7c7634fae9b50190dc86807aa68a
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-27 20:58:29 +0800

    header styling

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,13 +1,14 @@
 <header class="header">
-		<div class="hamburger-lines">
-			<span class="line line1"></span>
-			<span class="line line2"></span>
-			<span class="line line3"></span>
-		</div>
+	<div class="hamburger-lines">
+		<span class="line line1"></span>
+		<span class="line line2"></span>
+		<span class="line line3"></span>
+	</div>
 
+	<div class="right">
 		<?php if (!isset($_SESSION["is_logged_in"]) || !$_SESSION["is_logged_in"]): ?>
 			<a class="login" href="login">Login</a>
 		<?php endif; ?>
-
 		<button type="button" id="theme-toggle"></button>
-	</header>
+	</div>
+</header>
