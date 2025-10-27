e77c3c2c87f9217cc8ef062cddc0a02e04cb837a
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-27 20:54:52 +0800

    added authenticate and update controllers

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -35,41 +35,61 @@ class UserController extends User
 		}
 	}
 
-	// ADMIN-ONLY
-
-	public function showAll()
+	public function authenticate($username, $password)
 	{
-		$this->requireAdmin();
+		$errors = [];
 
-		return $this->getAllUsers();
-	}
+		if (!$username)
+			$errors["username"] = "Please enter your username.";
 
-	// OTHERS
-	public function show($id)
-	{
-		return $this->getById($id);
-	}
+		if (!$password)
+			$errors["password"] = "Please enter your password.";
 
-	public function register(
-		$first_name,
-		$last_name,
-		$middle_name,
-		$username,
-		$password
-	) {
-		if ($this->userExists($username)) {
-			return ["error" => "Username already taken."];
+		if ($errors)
+			return ["errors" => $errors];
+
+		$userData = $this->getByUsername($username);
+
+		if (!$userData || !password_verify($password, $userData["password"])) {
+			// Error messages are vague so that pentesters won't try to
+			// bruteforce usernames until it stops saying "User not found".
+			$errors["general"] = "Incorrect credentials.";
+			return ["errors" => $errors];
 		}
 
-		$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-		$created = $this->createUser(
-			$first_name,
-			$last_name,
-			$middle_name,
-			$username,
-			$hashedPassword
-		);
+		return ["success" => true, "user" => $userData];
+	}
 
+	public function getAll()
+	{
+		$this->requireAdmin();
+		return $this->userModel->getAll();
+	}
+
+	public function getById($id)
+	{
+		return $this->userModel->getById($id);
+	}
+
+	public function register($first_name, $last_name, $middle_name, $username, $password, $role)
+	{
+		if ($this->userModel->exists($username))
+			return ["error" => "Username already taken."];
+
+		$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+		$created = $this->userModel->create($first_name, $last_name, $middle_name, $username, $hashedPassword, $role);
 		return $created ? ["success" => true] : ["error" => "Failed to create account."];
+	}
+
+	public function update($id, $first_name, $last_name, $middle_name, $username, $password, $role)
+	{
+		$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+		$updated = $this->userModel->update($id, $first_name, $last_name, $middle_name, $username, $hashedPassword, $role);
+		return $updated ? ["success" => true] : ["error" => "Failed to update account."];
+	}
+
+	public function delete($id)
+	{
+		return $this->userModel->delete($id);
 	}
 }
