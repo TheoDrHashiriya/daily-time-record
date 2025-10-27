ea87610d170ec0428d5925d67d4363126027e904
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-27 20:55:39 +0800

    refactored and added new CRUD related functions

--- app/models/DailyTimeRecord.php
+++ app/models/DailyTimeRecord.php
@@ -32,6 +32,70 @@ class DailyTimeRecord extends Database
 		return $query->fetch() !== false;
 	}
 
+	public function getAll()
+	{
+		$sql = "SELECT dtr.*, CONCAT(u.first_name, ' ', u.last_name) AS user
+				  FROM {$this->table} dtr
+				  JOIN user u ON dtr.user_id = u.id
+				  ORDER BY dtr.record_date DESC;";
+		$query = $this->connect()->prepare($sql);
+		$query->execute();
+		return $query->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function getLast($id)
+	{
+		$sql = "SELECT *
+				  FROM {$this->table} dtr
+				  JOIN user u ON dtr.user_id = u.id
+				  WHERE dtr.user_id = :id
+				  ORDER BY dtr.record_date DESC 1;";
+		$query = $this->connect()->prepare($sql);
+		$query->bindParam(":id", $id);
+		$query->execute();
+		return $query->fetch(PDO::FETCH_ASSOC);
+	}
+
+	public function getByUserId($user_id)
+	{
+		$sql = "SELECT dtr.*, CONCAT(u.first_name, ' ', u.last_name) AS user
+				  FROM {$this->table} dtr
+				  JOIN user u ON dtr.user_id = u.id
+				  WHERE dtr.user_id = :id
+				  ORDER BY dtr.record_date DESC;";
+		$query = $this->connect()->prepare($sql);
+		$query->bindParam(":id", $user_id);
+		$query->execute();
+		return $query->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function getByUserIdAndDate($user_id, $date)
+	{
+		$sql = "SELECT dtr.*, CONCAT(u.first_name, ' ', u.last_name) AS user
+				  FROM {$this->table} dtr
+				  JOIN user u ON dtr.user_id = u.id
+				  WHERE dtr.user_id = :id AND dtr.date = :date
+				  ORDER BY dtr.record_date DESC;";
+		$query = $this->connect()->prepare($sql);
+		$query->bindParam(":id", $user_id);
+		$query->bindParam(":date", $date);
+		$query->execute();
+		return $query->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function getRecordUsername($id)
+	{
+		$sql = "SELECT u.username
+				  FROM {$this->table} dtr
+				  JOIN user u ON dtr.user_id = u.id
+				  WHERE dtr.id = :id
+				  ORDER BY dtr.record_date DESC;";
+		$query = $this->connect()->prepare($sql);
+		$query->bindParam(":id", $id);
+		$query->execute();
+		return $query->fetchAll(PDO::FETCH_ASSOC);
+	}
+
 	public function hasUnclosedRecord($id)
 	{
 
@@ -58,54 +122,47 @@ class DailyTimeRecord extends Database
 		return $query->execute();
 	}
 
-	public function getAll()
+	public function create($user_id, $record_date, $time_in, $time_out)
 	{
-		$sql = "SELECT dtr.*, CONCAT(u.first_name, ' ', u.last_name) AS user
-				  FROM {$this->table} dtr
-				  JOIN user u ON dtr.user_id = u.id
-				  ORDER BY dtr.record_date DESC;";
+		$sql = "INSERT INTO {$this->table} (user_id, record_date, time_in, time_out)
+				  VALUES (:user_id, :record_date, :time_in, :time_out);";
 		$query = $this->connect()->prepare($sql);
-		$query->execute();
-		return $query->fetchAll(PDO::FETCH_ASSOC);
+		$query->bindParam(":user_id", $user_id);
+		$query->bindParam(":record_date", $record_date);
+		$query->bindParam(":time_in", $time_in);
+		$query->bindParam(":time_out", $time_out);
+		return $query->execute();
 	}
 
-	public function getLast($id)
+	public function update($id, $user_id, $record_date, $time_in, $time_out)
 	{
-		$sql = "SELECT *
-				  FROM {$this->table} dtr
-				  JOIN user u ON dtr.user_id = u.id
-				  WHERE dtr.user_id = :id
-				  ORDER BY dtr.record_date DESC 1;";
+		$sql = "UPDATE {$this->table}
+				  SET user_id = :user_id, record_date = :record_date, time_in = :time_in, time_out = :time_out
+				  WHERE id = :id;";
 		$query = $this->connect()->prepare($sql);
 		$query->bindParam(":id", $id);
-		$query->execute();
-		return $query->fetch(PDO::FETCH_ASSOC);
+		$query->bindParam(":user_id", $user_id);
+		$query->bindParam(":record_date", $record_date);
+		$query->bindParam(":time_in", $time_in);
+		$query->bindParam(":time_out", $time_out);
+		return $query->execute();
 	}
 
-	public function getByUserId($id)
+	public function delete($id)
 	{
-		$sql = "SELECT dtr.*, CONCAT(u.first_name, ' ', u.last_name) AS user
-				  FROM {$this->table} dtr
-				  JOIN user u ON dtr.user_id = u.id
-				  WHERE dtr.user_id = :id
-				  ORDER BY dtr.record_date DESC;";
+		$sql = "DELETE FROM {$this->table}
+				  WHERE id = :id;";
 		$query = $this->connect()->prepare($sql);
 		$query->bindParam(":id", $id);
-		$query->execute();
-		return $query->fetchAll(PDO::FETCH_ASSOC);
+		return $query->execute();
 	}
 
-	public function getByUserIdAndDate($id, $date)
+	public function deleteAllFromUser($userId)
 	{
-		$sql = "SELECT dtr.*, CONCAT(u.first_name, ' ', u.last_name) AS user
-				  FROM {$this->table} dtr
-				  JOIN user u ON dtr.user_id = u.id
-				  WHERE dtr.user_id = :id AND dtr.date
-				  ORDER BY dtr.record_date DESC;";
+		$sql = "DELETE FROM {$this->table}
+				  WHERE user_id = :user_id;";
 		$query = $this->connect()->prepare($sql);
-		$query->bindParam(":id", $id);
-		$query->bindParam(":date", $date);
-		$query->execute();
-		return $query->fetchAll(PDO::FETCH_ASSOC);
+		$query->bindParam(":user_id", $userId);
+		return $query->execute();
 	}
 }
