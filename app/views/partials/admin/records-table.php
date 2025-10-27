33059ba4f0bf0f2139679c5a3ffde82138adc197
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-27 21:00:04 +0800

    added new actions and added username column (not working yet)

--- app/views/partials/admin/records-table.php
+++ app/views/partials/admin/records-table.php
@@ -7,6 +7,7 @@
 					<tr>
 						<th>Date</th>
 						<th>User</th>
+						<th>Username</th>
 						<th>Time In</th>
 						<th>Time Out</th>
 						<th>Actions</th>
@@ -18,6 +19,7 @@
 						<tr>
 							<td><?= htmlspecialchars($row["record_date"]) ?></td>
 							<td><?= htmlspecialchars($row["user"]) ?></td>
+							<td><?= htmlspecialchars($row["username"]) ?></td>
 							<td>
 								<?= $row["time_in"] ? htmlspecialchars(
 									date("h:i A", strtotime($row["time_in"]))
@@ -29,7 +31,16 @@
 								) : "<em>No time out record.</em>" ?>
 							</td>
 							<td>
-								<a href="delete-record/"></a>
+								<form action="edit-record" method="post" class="action">
+									<input type="hidden" name="id" value="<?= $row["id"] ?>">
+									<button type="submit">Edit</button>
+								</form>
+
+								<form action="delete-record" method="post" class="action">
+									<input type="hidden" name="id" value="<?= $row["id"] ?>">
+									<button class="danger" type="submit"
+										onclick="return confirm('Are you sure you want to delete the current record for <?= $row['user'] ?>?')">Delete</button>
+								</form>
 							</td>
 						</tr>
 					<?php endforeach; ?>
