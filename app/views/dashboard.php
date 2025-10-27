0ba43516a4c7962bd205ab02f72c1d7ad4978e00
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-27 20:56:47 +0800

    added mobile hamburger

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -9,39 +9,46 @@
 	<script src="<?= BASE_URL ?>public/js/theme.js" defer></script>
 	<script src="<?= BASE_URL ?>public/js/clock.js" defer></script>
 	<script src="<?= BASE_URL ?>public/js/page-toggle.js" defer></script>
+	<script src="<?= BASE_URL ?>public/js/sidebar-toggle.js" defer></script>
 </head>
 
 <body>
-	<?= include "partials/header.php" ?>
-	<?= include "partials/sidebar.php" ?>
+	<div class="dashboard-container">
+		<?php include "partials/header.php"; ?>
+		<?php include "partials/sidebar.php"; ?>
 
-	<main class="main section" id="home-section">
-		<?= include "partials/welcome-logout-card.php" ?>
-		<?= include "partials/time-card.php" ?>
+		<main class="main section" id="home-section">
+			<?php include "partials/welcome-logout-card.php"; ?>
+			<?php include "partials/time-card.php"; ?>
 
-		<?php if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"]) {
-			include "partials/records-newest-table.php";
-			if ($userRole === "admin")
-				include "more admin stuff please";
+			<?php if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"]) {
+				include "partials/records-newest-table.php";
+				if ($userRole === "admin") {
+					// empty for now
+				}
 
-			if ($userRole == "employee")
-				include "partials/time-buttons.php";
-		} ?>
-	</main>
-
-	<?php if (
-		isset($_SESSION["is_logged_in"])
-		&& $_SESSION["is_logged_in"]
-		&& $userRole === "admin"
-	): ?>
-		<main class="main section" id="users-section">
-			<?= include "partials/admin/users-table.php"; ?>
+				if ($userRole == "employee")
+					include "partials/time-buttons.php";
+			} ?>
 		</main>
 
-		<main class="main section" id="records-section">
-			<?= include "partials/admin/records-table.php"; ?>
-		</main>
-	<?php endif; ?>
+		<?php if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"]):
+			if ($userRole === "admin"): ?>
+				<main class="main section" id="users-section">
+					<?php include "partials/admin/users-table.php"; ?>
+				</main>
+
+				<main class="main section" id="records-section">
+					<?php include "partials/admin/records-table.php"; ?>
+				</main>
+			<?php endif;
+			if ($userRole === "employee"): ?>
+				<main class="main section" id="records-section">
+					<?php include "partials/records-table.php"; ?>
+				</main>
+			<?php endif;
+		endif; ?>
+	</div>
 </body>
 
 </html>
