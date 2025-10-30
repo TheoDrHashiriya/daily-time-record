230a5431f62b41b33e2194a268b055aecb482732
TheoDrHashiriya <theotrisha@gmail.com>
2025-10-30 11:31:44 +0800

    clock updates date every midnight and it shows both date and time separately

--- app/views/partials/time-card.php
+++ app/views/partials/time-card.php
@@ -1,4 +1,4 @@
 <div class="card" id="time">
-	<h2 class="title header">Time Now</h2>
-	<h2 id="clock"></h2>
+	<h3 id="date-text"></h4>
+	<h2 id="time-text"></h2>
 </div>
--- public/js/clock.js
+++ public/js/clock.js
@@ -1,18 +1,27 @@
-function updateClock() {
+function updateDate() {
+	const date = document.getElementById("date-text");
+	const now = new Date();
+	const dateString = now.toLocaleDateString("en-US", {
+		dateStyle: "full",
+	});
+	date.textContent = dateString;
+}
+
+function updateTime() {
+const time = document.getElementById("time-text");
 	const now = new Date();
 	const timeString = now.toLocaleTimeString("en-US", {
-		weekday: "long",
-		month: "long",
-		day: "numeric",
-		year: "numeric",
 		hour: "numeric",
 		minute: "numeric",
 		second: "numeric",
 		hour12: true,
 	});
-	document.getElementById("clock").textContent = timeString;
+	time.textContent = timeString;
+
+	if (timeString.startsWith("12:00 AM"))
+		updateDate();
 }
 
-updateClock();
-
-setInterval(updateClock, 1000);
+updateDate();
+updateTime();
+setInterval(updateTime, 1000);
