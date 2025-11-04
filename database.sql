2d7a73886443ba7ced95a917ece5f209b4d7f3dd
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-04 10:21:19 +0800

    revamped sql database to not use reserved keywords as attributes and added not null constraints to relevant attributes

--- database.sql
+++ database.sql
@@ -1,50 +1,52 @@
 CREATE DATABASE theonary;
 USE theonary;
-CREATE TABLE user(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	middle_name VARCHAR(50),
-	username VARCHAR(50) UNIQUE,
-	password VARCHAR(255),
-	created_by INT NULL,
-	FOREIGN KEY (created_by) REFERENCES user(id),
-	role INT DEFAULT 2,
-	FOREIGN KEY (role) REFERENCES user_role(id)
-);
 CREATE TABLE user_role(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(50) UNIQUE
+	role_name VARCHAR(50) UNIQUE NOT NULL
+);
+CREATE TABLE user(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	middle_name VARCHAR(50) NULL,
+	username VARCHAR(100) UNIQUE NOT NULL,
+	hashed_password VARCHAR(255) NOT NULL,
+	created_by INT NULL,
+	FOREIGN KEY (created_by) REFERENCES user(id),
+	user_role INT DEFAULT 2,
+	FOREIGN KEY (user_role) REFERENCES user_role(id)
+);
+CREATE TABLE event_record_type(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	type_name VARCHAR(50) UNIQUE NOT NULL
 );
 CREATE TABLE event_record(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	record_date DATE NOT NULL,
-	time DATETIME NOT NULL,
-	type INT NOT NULL,
-	FOREIGN KEY (type) REFERENCES event_record_type(id),
+	event_time DATETIME NOT NULL,
+	event_type INT NOT NULL,
+	FOREIGN KEY (event_type) REFERENCES event_record_type(id),
 	user_id INT NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES user(id)
 );
-CREATE TABLE event_record_type(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(50) UNIQUE
-);
 -- INITIAL INSERTIONS
-INSERT INTO user_role (id, name) -- Explicit IDs so the user role defaulting to 2 always works
+-- Explicit IDs so the user role defaulting to 2 always works
+INSERT INTO user_role (id, role_name)
 VALUES (1, 'admin'),
 	(2, 'employee'),
 	(3, 'manager');
-INSERT INTO event_record_type(name)
-VALUES ('time_in'),
-	('time_out');
+-- Same idea here, to be certain when querying
+INSERT INTO event_record_type(id, type_name)
+VALUES (1, 'time_in'),
+	(2, 'time_out');
 -- Add the default admin account
 INSERT INTO user (
 		first_name,
 		last_name,
 		middle_name,
 		username,
-		password,
-		role
+		hashed_password,
+		user_role
 	)
 VALUES (
 		'John',
@@ -55,4 +57,4 @@ VALUES (
 		1
 	);
 -- To generate new password hashes, use this:
--- php -r "echo password_hash("YourPassword", PASSWORD_DEFAULT) . PHP_EOL;"
+-- php -r "echo password_hash('YourPassword', PASSWORD_DEFAULT) . PHP_EOL;"
