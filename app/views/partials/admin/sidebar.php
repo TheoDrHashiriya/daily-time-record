5ad5e062d97422e0c1aeff4caccb2d851c172154
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-05 10:46:55 +0800

    moved relevant partials to admin

---
+++ app/views/partials/admin/sidebar.php
@@ -0,0 +1,11 @@
+<section id="sidebar" class="sidebar">
+	<div class="top">
+		<a class="nav" href="#" data-target="home-section">Home</a>
+		<a class="nav" href="#" data-target="records-section">Records</a>
+		<a class="nav" href="#" data-target="users-section">Users</a>
+	</div>
+
+	<div class="bottom">
+		<a id="logout" href="logout">Logout</a>
+	</div>
+</section>
