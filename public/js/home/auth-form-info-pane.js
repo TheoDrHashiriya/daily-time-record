eac6b3f37c5f3276bada87c5c59b3f9246acacf4
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-11 11:04:46 +0800

    auto logout after displaying info pane

--- public/js/home/auth-form-info-pane.js
+++ public/js/home/auth-form-info-pane.js
@@ -2,6 +2,9 @@ window.addEventListener("DOMContentLoaded", () => {
 	const infoPane = document.getElementById("info-pane");
 	if (infoPane && infoPane.textContent.trim() !== '') {
 		infoPane.style.display = "block";
-		setTimeout(() => infoPane.style.display = "none", 5000);
+		setTimeout(() => {
+			infoPane.style.display = "none";
+			window.location.href = "logout";
+		 }, 5000);
 	}
 });
