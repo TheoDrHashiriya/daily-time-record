58653c8b88b4a3db216e1432d4bb6e571f7b48b0
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-11 11:01:46 +0800

    added new pdf helper functions via a PdfHelper class

---
+++ app/helpers/PdfHelper.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . "/../libraries/dompdf/autoload.inc.php";
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+class PdfHelper
+{
+	public static function initializeDompdf()
+	{
+		$options = new Options();
+		$options->set("isRemoteEnabled", true);
+
+		$dompdf = new Dompdf($options);
+		$dompdf->setPaper("A4", "portrait");
+		return $dompdf;
+	}
+
+	public static function generatePdf($html, $filename = "document.pdf")
+	{
+		$dompdf = PdfHelper::initializeDompdf();
+		$dompdf->loadHtml($html);
+		$dompdf->render();
+		$dompdf->stream($filename);
+	}
+
+	public static function generatePdfString($html)
+	{
+		$dompdf = PdfHelper::initializeDompdf();
+		$dompdf->loadHtml($html);
+		$dompdf->render();
+		return $dompdf;
+	}
+}
