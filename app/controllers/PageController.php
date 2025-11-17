b54328fd442289c052b4aff90b74903c4b5c7b73
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-17 22:13:31 +0800

    too many changes in one file, see working tree

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -1,19 +1,23 @@
 <?php
 require_once __DIR__ . "/../controllers/EventRecordController.php";
 require_once __DIR__ . "/../controllers/UserController.php";
+require_once __DIR__ . "/../controllers/NotificationController.php";
+require_once __DIR__ . "/../controllers/DepartmentController.php";
 
 class PageController
 {
 	private $erController;
 	private $userController;
+	private $notifController;
+	private $depController;
 
 	public function __construct()
 	{
 		$this->erController = new ERController();
 		$this->userController = new UserController();
+		$this->notifController = new NotificationController();
+		$this->depController = new DepartmentController();
 	}
-
-	// PDF HANDLING
 
 	public function previewAllEventsPdf()
 	{
@@ -29,6 +33,40 @@ class PageController
 
 		$dompdf = PdfHelper::generatePdfString($html);
 		$dompdf->stream("all-events.pdf", ["Attachment" => false]);
+		exit();
+	}
+
+	public function previewAllNotificationsPdf()
+	{
+		$this->userController->requireLogin();
+		$this->userController->requireAdmin();
+
+		$notifications = $this->notifController->getAll();
+
+		ob_start();
+		require __DIR__ . "/../views/partials/pdf-styles.php";
+		require __DIR__ . "/../views/partials/notifications-table.php";
+		$html = ob_get_clean();
+
+		$dompdf = PdfHelper::generatePdfString($html);
+		$dompdf->stream("all-notifications.pdf", ["Attachment" => false]);
+		exit();
+	}
+
+	public function previewAllDepartmentsPdf()
+	{
+		$this->userController->requireLogin();
+		$this->userController->requireAdmin();
+
+		$departments = $this->depController->getAll();
+
+		ob_start();
+		require __DIR__ . "/../views/partials/pdf-styles.php";
+		require __DIR__ . "/../views/partials/departments-table.php";
+		$html = ob_get_clean();
+
+		$dompdf = PdfHelper::generatePdfString($html);
+		$dompdf->stream("all-departments.pdf", ["Attachment" => false]);
 		exit();
 	}
 
@@ -70,12 +108,26 @@ class PageController
 	{
 		$this->userController->requireLogin();
 		$this->erController->timeIn($_SESSION["user_id"]);
+
+		$now = GlobalHelper::getCurrentDate();
+		$this->notifController->create(
+			"Time In",
+			$_SESSION["username"] . " has timed in on " . GlobalHelper::formatDate($now) . ", at " . GlobalHelper::formatTime($now) . ".",
+			$_SESSION["user_id"]
+		);
 	}
 
 	public function timeOut()
 	{
 		$this->userController->requireLogin();
 		$this->erController->timeOut($_SESSION["user_id"]);
+
+		$now = GlobalHelper::getCurrentDate();
+		$this->notifController->create(
+			"Time Out",
+			$_SESSION["username"] . " has timed out on " . GlobalHelper::formatDate($now) . ", at " . GlobalHelper::formatTime($now) . ".",
+			$_SESSION["user_id"]
+		);
 	}
 
 	public function authenticate()
@@ -145,6 +197,9 @@ class PageController
 		$this->userController->requireAdmin();
 		$records = $this->erController->getAll();
 		$users = $this->userController->getAll();
+		$notifications = $this->notifController->getAll();
+		$notifications_unread = $this->notifController->getAllUnread();
+		$departments = $this->depController->getAll();
 		$kpiData = $this->getKpiData();
 		require __DIR__ . "/../views/admin/dashboard.php";
 	}
@@ -153,9 +208,9 @@ class PageController
 	{
 		$kpiData["events_total"] = $this->erController->getTotal();
 		$kpiData["events_unclosed"] = $this->erController->getTotalUnclosed();
-
 		$kpiData["users_total"] = $this->userController->getTotal();
-
+		$kpiData["notifications_total"] = $this->notifController->getTotal();
+		$kpiData["departments_total"] = $this->depController->getTotal();
 		return $kpiData;
 	}
 
@@ -284,11 +339,25 @@ class PageController
 
 	// RECORDS
 
+	public function editRecord()
+	{
+	}
+
 	public function deleteRecord($id)
 	{
 		$this->userController->requireLogin();
 		$this->userController->requireAdmin();
 		$this->erController->delete($id);
+		$this->redirectToDashboard();
+		exit();
+	}
+
+	// NOTIFICATIONS
+	public function deleteNotification($id)
+	{
+		$this->userController->requireLogin();
+		$this->userController->requireAdmin();
+		$this->notifController->delete($id);
 		$this->redirectToDashboard();
 		exit();
 	}
