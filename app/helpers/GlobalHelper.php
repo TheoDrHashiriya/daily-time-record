4fa4a50616a138363dd7a3cf37e0ab38cef34c9b
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-17 22:14:15 +0800

    added getCurrentDate and sortArrayByColumn helpers

--- app/helpers/GlobalHelper.php
+++ app/helpers/GlobalHelper.php
@@ -6,6 +6,13 @@ class GlobalHelper
 		return ucwords(str_replace('_', ' ', $text));
 	}
 
+	// DATE & TIME
+
+	public static function getCurrentDate()
+	{
+		return date("Y-m-d H:i:s");
+	}
+
 	public static function formatDate($date)
 	{
 		return date("D, M j, Y", strtotime($date));
@@ -13,11 +20,20 @@ class GlobalHelper
 
 	public static function formatTime($time)
 	{
-		return date("h:i A", strtotime($time));
+		return date("h:i:s A", strtotime($time));
 	}
 
 	public static function formatEventType($type_name)
 	{
 		return strtoupper(str_replace("time_", '', $type_name));
+	}
+
+	// SORTING
+
+	public static function sortArrayByColumn($array, $column_name)
+	{
+		$column = array_column($array, $column_name);
+		array_multisort($column, SORT_DESC, $array);
+		return $array;
 	}
 }
