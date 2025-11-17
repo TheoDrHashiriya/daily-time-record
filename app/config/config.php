11d0dd4db8bccbae5a491504865f27a167ca266a
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-17 22:14:38 +0800

    moved db credentials to separate config file

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+date_default_timezone_set("Asia/Manila");
+
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_NAME = "theonary";
+const DB_PORT = 3306;
+const BASE_URL = "/theonary/";
--- app/models/Database.php
+++ app/models/Database.php
@@ -1,16 +1,17 @@
 <?php
 class Database
 {
-	private $host = "localhost";
-	private $username = "root";
-	private $password = "";
-	private $dbName = "theonary";
+	private $host = DB_HOST;
+	private $user = DB_USER;
+	private $password = DB_PASS;
+	private $dbName = DB_NAME;
+	private $dbPort = DB_PORT;
 	protected $conn;
 	public function connect()
 	{
 		$this->conn = new PDO(
 			"mysql:host=$this->host;dbname=$this->dbName",
-			$this->username,
+			$this->user,
 			$this->password
 		);
 		return $this->conn;
