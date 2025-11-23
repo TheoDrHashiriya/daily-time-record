c23c620ee7590577a603824ec2eba8b7e4249284
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-23 18:17:10 +0800

    removed pdfhelper require

--- app/router.php
+++ app/router.php
@@ -3,7 +3,6 @@ session_start();
 
 require_once __DIR__ . "/config/config.php";
 require_once __DIR__ . "./../vendor/autoload.php";
-require_once __DIR__ . "/helpers/PdfHelper.php";
 require_once __DIR__ . "/helpers/GlobalHelper.php";
 require_once __DIR__ . "/controllers/PageController.php";
 
