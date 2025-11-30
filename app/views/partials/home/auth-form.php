1cd8a2ce6d2c36aa35a39681087fdcedc0539511
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-30 21:49:36 +0800

    minor refactor of auth form styling and some minor changes

--- app/views/partials/home/auth-form.php
+++ app/views/partials/home/auth-form.php
@@ -1,17 +1,17 @@
-<div class="form-box" id="login-form">
-	<h2 class="title">Authenticate</h2>
+<div class="card">
+	<h2 class="header">Authenticate</h2>
 	<form action="authenticate" method="post">
 		<label for="username">Username</label>
 		<input value="<?= htmlspecialchars($username ?? "") ?>" placeholder="Username" type="text" name="username"
 			id="username">
 		<p class="error"><?= $errors["username"] ?? "" ?></p>
 
-		<label for="title">Password</label>
+		<label for="password">Password</label>
 		<input placeholder="Password" type="password" name="password" id="password">
 		<p class="error"><?= $errors["password"] ?? "" ?></p>
 
 		<p class="error-general"><?= $errors["general"] ?? "" ?></p>
 
-		<button class="login" type="submit">Authenticate</button>
+		<button class="main" type="submit">Authenticate</button>
 	</form>
 </div>
