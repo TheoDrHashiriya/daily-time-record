8ffbd2e44db5e873a6dbd11d8fdb42b0d28bd3c2
TheoDrHashiriya <theotrisha@gmail.com>
2025-11-30 21:47:02 +0800

    also give formatted data to dashboard page

--- app/services/DashboardService.php
+++ app/services/DashboardService.php
@@ -1,39 +1,82 @@
 <?php
 namespace App\Services;
-use App\Models\User;
-use App\Models\EventRecord;
 use App\Models\Department;
+use App\Models\EventRecord;
 use App\Models\Notification;
+use App\Models\User;
+use App\Models\UserRole;
 
 class DashboardService
 {
-	private $userModel;
-	private $recordModel;
-	private $notificationModel;
 	private $departmentModel;
+	private $notificationModel;
+	private $recordModel;
+	private $userModel;
+	private $userRoleModel;
 
 	public function __construct()
 	{
-		$this->userModel = new User();
-		$this->recordModel = new EventRecord();
-		$this->notificationModel = new Notification();
 		$this->departmentModel = new Department();
+		$this->notificationModel = new Notification();
+		$this->recordModel = new EventRecord();
+		$this->userModel = new User();
+		$this->userRoleModel = new UserRole();
 	}
 
 	public function getAllData()
 	{
-		$users = $this->userModel->getAll();
-		$records = $this->recordModel->getAll();
+		$departments = $this->departmentModel->getAll();
 		$notifications = $this->notificationModel->getAll();
 		$notifications_unread = $this->notificationModel->getAllUnread();
-		$departments = $this->departmentModel->getAll();
+		$records = $this->recordModel->getAll();
+		$record_types = $this->recordModel->getAllTypes();
+		$users = $this->userModel->getAll();
+		$user_roles = $this->userRoleModel->getAll();
+
+		// Format Data
+		foreach ($notifications as &$notification) {
+			$notification["created_by_full_name_formatted"] = FormattingService::formatFullName(
+				$notification["first_name"],
+				$notification["middle_name"],
+				$notification["last_name"]
+			);
+		}
+		unset($notification);
+
+		foreach ($records as &$record) {
+			$record["type_name_formatted"] = FormattingService::formatEventType($record["type_name"]);
+			$record["event_date_formatted"] = FormattingService::formatDate($record["event_time"]);
+			$record["event_time_formatted"] = FormattingService::formatTime($record["event_time"]);
+		}
+		unset($record);
+
+		foreach ($record_types as &$type) {
+			$type["type_name_formatted"] = FormattingService::formatEventType($type["type_name"]);
+		}
+		unset($type);
+
+		foreach ($users as &$user) {
+			$user["full_name_formatted"] = FormattingService::formatFullName(
+				$user["first_name"],
+				$user["middle_name"],
+				$user["last_name"],
+			);
+		}
+		unset($user);
+
+		foreach ($user_roles as &$role) {
+			$role["role_name_formatted"] = FormattingService::formatText($role["role_name"]);
+		}
+		unset($role);
 
 		return [
-			"users" => $users,
-			"records" => $records,
+			"departments" => $departments,
 			"notifications" => $notifications,
 			"notifications_unread" => $notifications_unread,
-			"departments" => $departments
+			"records" => $records,
+			"record_types" => $record_types,
+			"users" => $users,
+			"user_roles" => $user_roles,
 		];
 	}
 }
