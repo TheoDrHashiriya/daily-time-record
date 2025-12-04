06014becb0fd13ede9a45df82a4fc98fa400fc5d
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-04 19:52:10 +0800

    configuration overhaul: many constants stored in separate files, and implement main bootstrap file

---
+++ app/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+// Global Constants
+require_once __DIR__ . "/config/paths.php";
+require_once CONFIG_PATH . "/constants.php";
+
+// Composer autoload
+require_once VENDOR_PATH . "/autoload.php";
+
+// Configs
+require_once CONFIG_PATH . "/database.php";
+require_once CONFIG_PATH . "/email.php";
+
+// Timezone
+date_default_timezone_set(TIMEZONE);
+
+// Debugging
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+// Let's start!
+if (session_status() === PHP_SESSION_NONE)
+	session_start();
---
+++ app/config/paths.php
@@ -0,0 +1,18 @@
+<?php
+// FILE SYSTEM PATHS
+const BASE_PATH = __DIR__ . "/../..";
+
+const APP_PATH = BASE_PATH . "/app";
+const CONFIG_PATH = APP_PATH . "/config";
+const VIEWS_PATH = APP_PATH . "/views";
+
+const VENDOR_PATH = BASE_PATH . "/vendor";
+
+// PUBLIC URLS
+const BASE_URL = "/theonary";
+
+const PUBLIC_URL = BASE_URL . "/public";
+const CSS_URL = PUBLIC_URL . "/css";
+const JS_URL = PUBLIC_URL . "/js";
+
+const VENDOR_URL = BASE_URL . "/vendor";
