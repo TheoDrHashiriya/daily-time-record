7d9606a25bc986f5520b30299a38ca742117eef2
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-06 22:49:34 +0800

    add error and success modals

---
+++ app/views/components/modals/error.php
@@ -0,0 +1,9 @@
+<div class="modal-container" id="error">
+	<div class="modal">
+		<div class="row header">
+			<h3 class="header"><i class="symbol fa-solid fa-exclamation"></i>Error!</h3>
+			<button type="button" class="close-button icon"><i class="fa-solid fa-x"></i></button>
+		</div>
+		<p class="error-general"><?= $_SESSION["message"]["error"] ?></p>
+	</div>
+</div>
---
+++ app/views/components/modals/success.php
@@ -0,0 +1,9 @@
+<div class="modal-container" id="success">
+	<div class="modal">
+		<div class="row header">
+			<h3 class="header"><i class="symbol fa-solid fa-check"></i>Success!</h3>
+			<button type="button" class="close-button icon"><i class="fa-solid fa-x"></i></button>
+		</div>
+		<p class="success"><?= $_SESSION["message"]["success"] ?></p>
+	</div>
+</div>
