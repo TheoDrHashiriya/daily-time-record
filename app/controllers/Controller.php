161eae4df41b5e60fdbee898cbe382e20665d95f
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-06 22:46:44 +0800

    split renderView function into two functions into two in base controller

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -2,11 +2,18 @@
 namespace App\Controllers;
 class Controller
 {
-	protected function renderView(array $views, array $data = [])
+	protected function renderView($view, $data = [])
+	{
+		extract($data);
+		require_once VIEWS_PATH . '/' . $view . ".php";
+
+	}
+
+	protected function renderViews($views = [], $data = [])
 	{
 		extract($data);
 		foreach ($views as $i => $view) {
-			require_once __DIR__ . "/../views/" . $view;
+			require_once VIEWS_PATH . '/' . $view . ".php";
 		}
 	}
 }
