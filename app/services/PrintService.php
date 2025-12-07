980790ab60d504935457a2573c8ed2853e7a15f0
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-07 11:38:57 +0800

    rename PdfService to PrintService. custom fonts now work!

--- app/services/PrintService.php
+++ app/services/PrintService.php
@@ -2,20 +2,20 @@
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
-class PdfService
+class PrintService
 {
 
-	public static function streamPdf(string $filename, $viewPaths, array $data = [], bool $download = false)
+	public static function streamPdf(string $filename, $views, array $data = [], bool $download = false)
 	{
-		if (!is_array($viewPaths))
-			$viewPaths = [$viewPaths];
+		if (!is_array($views))
+			$views = [$views];
 
 		extract($data);
 
 		$html = '';
-		foreach ($viewPaths as $viewPath) {
+		foreach ($views as $view) {
 			ob_start();
-			require __DIR__ . "/../views/{$viewPath}";
+			require_once VIEWS_PATH . '/' . $view . ".php";
 			$html .= ob_get_clean();
 		}
 
@@ -27,7 +27,12 @@ class PdfService
 	public static function initializeDompdf()
 	{
 		$options = new Options();
+
+		$options->set("isHTML5ParserEnabled", true);
 		$options->set("isRemoteEnabled", true);
+
+		$options->setFontDir(realpath(FONTS_PATH));
+		$options->setFontCache(realpath(FONTS_PATH));
 
 		$dompdf = new Dompdf($options);
 		$dompdf->setPaper("A4", "portrait");
