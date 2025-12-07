2f5df96c8e7762bb92927fac24f26aa129ede064
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-07 11:35:27 +0800

    added new constant paths

--- app/config/paths.php
+++ app/config/paths.php
@@ -5,8 +5,10 @@ const BASE_PATH = __DIR__ . "/../..";
 const APP_PATH = BASE_PATH . "/app";
 const CONFIG_PATH = APP_PATH . "/config";
 const VIEWS_PATH = APP_PATH . "/views";
-
 const VENDOR_PATH = BASE_PATH . "/vendor";
+
+const PUBLIC_PATH = BASE_PATH . "/public";
+const FONTS_PATH = PUBLIC_PATH . "/fonts";
 
 // PUBLIC URLS
 const BASE_URL = "/theonary";
@@ -14,5 +16,4 @@ const BASE_URL = "/theonary";
 const PUBLIC_URL = BASE_URL . "/public";
 const CSS_URL = PUBLIC_URL . "/css";
 const JS_URL = PUBLIC_URL . "/js";
-
 const VENDOR_URL = BASE_URL . "/vendor";
