ed90e8b67399d90baa114df6a94bc762b9618a38
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-11 22:44:10 +0800

    sidebar links have GET style URIs

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -1,22 +1,22 @@
 <section id="sidebar" class="sidebar">
 	<div class="top">
-		<a class="nav" href="#" data-target="home-section">
+		<a class="nav" href="" data-target="home-section">
 			<i class="fa-regular fa-house"></i>Home
 		</a>
 		
-		<a class="nav" href="#" data-target="notifications-section">
-			<i class="fa-regular fa-bell"></i>Notifications
+		<a class="nav" href="?page=system-logs" data-target="system-logs-section">
+			<i class="fa-regular fa-bell"></i>System Logs
 		</a>
 
-		<a class="nav" href="#" data-target="records-section">
+		<a class="nav" href="?page=records" data-target="records-section">
 			<i class="fa-regular fa-calendar-check"></i>Records
 		</a>
 
-		<a class="nav" href="#" data-target="users-section">
+		<a class="nav" href="?page=users" data-target="users-section">
 			<i class="fa-regular fa-user"></i>Users
 		</a>
 		
-		<a class="nav" href="#" data-target="departments-section">
+		<a class="nav" href="?page=departments" data-target="departments-section">
 			<i class="fa-regular fa-building"></i>Departments
 		</a>
 
