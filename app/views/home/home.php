3d16b1a27e4f64ff2c4961188b9f5841761b4169
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-11 22:44:53 +0800

    refactored homepage view to utilize changes

--- app/views/home/home.php
+++ app/views/home/home.php
@@ -8,21 +8,41 @@
 	<link rel="stylesheet" href="<?= CSS_URL ?>/pages/home.css">
 	<!-- Theming script must start early to avoid flash of light mode -->
 	<script src="<?= JS_URL ?>/theme.js"></script>
-	<script src="<?= JS_URL ?>/home/auth-form-info-pane.js" defer></script>
-	<script src="<?= JS_URL ?>/home/auth-form-code.js" defer></script>
+	<script src="<?= JS_URL ?>/home/auth-form.js" defer></script>
 	<script src="<?= JS_URL ?>/ajax-handler.js" defer></script>
-	<script src="<?= JS_URL ?>/clock.js" defer></script>
-	<?php if (!empty($_SESSION["code_is_admin"])): ?>
-		<script src="<?= JS_URL ?>/modal.js" defer></script>
-		<script src="<?= JS_URL ?>/home/show-login-modal.js" defer></script>
-	<?php endif ?>
+	<script src="<?= JS_URL ?>/modal.js" defer></script>
+	<script src="<?= JS_URL ?>/time.js" defer></script>
+
+	<?php if (isset($_SESSION["user_number_is_admin"])):
+		if ($_SESSION["user_number_is_admin"] === true): ?>
+			<script>document.addEventListener("DOMContentLoaded", () => showLoginUsernamePasswordModal())</script>
+		<?php elseif ($_SESSION["user_number_is_admin"] === false): ?>
+			<script src="<?= JS_URL ?>/jsqr/dist/jsQR.js" defer></script>
+			<script src="<?= JS_URL ?>/home/qr-code.js" defer></script>
+			<script>document.addEventListener("DOMContentLoaded", () => showLoginQrCodeModal())</script>
+		<?php endif;
+	endif ?>
 
 	<link rel="stylesheet" href="<?= VENDOR_URL ?>/fortawesome/font-awesome/css/all.min.css" />
 </head>
 
 <body>
 	<!-- MODALS -->
-	<?php include VIEWS_PATH . "/components/modals/admin-login.php" ?>
+	<?php include VIEWS_PATH . "/components/modals/success.php"; ?>
+	<?php include VIEWS_PATH . "/components/modals/info.php"; ?>
+	<?php include VIEWS_PATH . "/components/modals/error.php"; ?>
+
+	<?php if (isset($_SESSION["message"]["success"])): ?>
+		<script>document.addEventListener("DOMContentLoaded", () => showSuccessModal())</script>
+	<?php elseif (isset($_SESSION["message"]["info"])): ?>
+		<script>document.addEventListener("DOMContentLoaded", () => showInfoModal())</script>
+	<?php elseif (isset($_SESSION["message"]["error"])): ?>
+		<script>document.addEventListener("DOMContentLoaded", () => showErrorModal())</script>
+	<?php endif;
+	unset($_SESSION["message"]) ?>
+
+	<?php include VIEWS_PATH . "/components/modals/login-qr-code.php" ?>
+	<?php include VIEWS_PATH . "/components/modals/login-username-password.php" ?>
 
 	<header>
 		<div class="left">
@@ -45,23 +65,9 @@
 				<?php include VIEWS_PATH . "/components/cards/time.php" ?>
 			</section>
 
-			<section id="info-pane" style="display: none;">
-				<div class="card" id="info">
-					<h4>
-						<?php
-						if (!isset($_SESSION["error"]))
-							echo $_SESSION["success"] ?? "";
-						echo $_SESSION["error"] ?? "";
-						?>
-					</h4>
-				</div>
-			</section>
-
 			<section id="authentication-pane">
-				<?php include VIEWS_PATH . "/components/forms/auth-form.php" ?>
-		</div>
-		</section>
-		</div>
+				<?php include VIEWS_PATH . "/components/forms/authenticate.php" ?>
+			</section>
 	</main>
 </body>
 
