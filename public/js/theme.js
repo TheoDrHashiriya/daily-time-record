466cef50d807ae611f6c34737d3375c37a609a02
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-13 12:40:59 +0800

    theme button and notification bell now has hover color

--- public/js/theme.js
+++ public/js/theme.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	const themeToggle = document.getElementById("theme-toggle");
 
-	themeToggle.innerHTML = savedTheme === "dark" ? '<i class="fa-solid fa-sun"></i>' : '<i class="fa-solid fa-moon"></i>';
+	themeToggle.innerHTML = savedTheme === "dark" ? '<i class="button fa-solid fa-sun"></i>' : '<i class="button fa-solid fa-moon"></i>';
 
 	themeToggle.addEventListener("click", () => {
 		const currentTheme = document.documentElement.getAttribute("data-theme");
 		const newTheme = currentTheme === "dark" ? "light" : "dark";
 		document.documentElement.setAttribute("data-theme", newTheme);
 		localStorage.setItem("theme", newTheme);
-		themeToggle.innerHTML = newTheme === "dark" ? '<i class="fa-solid fa-sun"></i>' : '<i class="fa-solid fa-moon"></i>';
+		themeToggle.innerHTML = newTheme === "dark" ? '<i class="button fa-solid fa-sun"></i>' : '<i class="button fa-solid fa-moon"></i>';
 	});
 });
