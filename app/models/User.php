eedf4a0719f7160030d6d9d546a8a75c549ca9ce
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-14 20:06:24 +0800

    User model: fixed update and many other refactors, fixes, and renames

--- app/models/User.php
+++ app/models/User.php
@@ -15,8 +15,8 @@ class User
 	public function userNumberMatchesId($user_number, $id)
 	{
 		$sql = "
-			SELECT COUNT(*)
-			FROM " . USER_TABLE . "
+			SELECT 1
+			FROM user
 			WHERE id = :id AND user_number = :user_number
 			LIMIT 1";
 		$query = $this->db->connect()->prepare($sql);
@@ -26,9 +26,35 @@ class User
 		return $query->fetchColumn() > 0;
 	}
 
+	public function qrStringExists($qr_string)
+	{
+		$sql = "SELECT 1
+				  FROM qr_code
+				  WHERE qr_string = :qr_string
+				  LIMIT 1";
+		$query = $this->db->connect()->prepare($sql);
+		$query->bindParam(":qr_string", $qr_string);
+		$query->execute();
+		return $query->fetchColumn() > 0;
+	}
+
+	public function generateQrString($user_id, $qr_string)
+	{
+		$sql = "INSERT INTO qr_code (user_id, qr_string)
+				  VALUES (:user_id, :qr_string)";
+		$query = $this->db->connect()->prepare($sql);
+		$query->bindParam(":user_id", $user_id);
+		$query->bindParam(":qr_string", $qr_string);
+		$query->execute();
+		return $query->fetch(PDO::FETCH_ASSOC) ?: null;
+	}
+
 	public function getById($id)
 	{
-		$sql = "SELECT * FROM " . USER_TABLE . " WHERE id = :id LIMIT 1";
+		$sql = "SELECT u.*, qr.qr_string
+				  FROM user u
+				  LEFT JOIN qr_code qr ON u.id = qr.user_id
+				  WHERE u.id = :id LIMIT 1";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":id", $id);
 		$query->execute();
@@ -37,28 +63,29 @@ class User
 
 	public function getByUserNumber($user_number)
 	{
-		$sql = "SELECT * FROM " . USER_TABLE . " WHERE user_number = :user_number LIMIT 1";
+		$sql = "SELECT * FROM user WHERE user_number = :user_number LIMIT 1";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":user_number", $user_number);
-		$query->execute();
-		return $query->fetch(PDO::FETCH_ASSOC) ?: null;
-	}
-	public function getByQrCode($qr_code)
-	{
-		$sql = "SELECT * FROM " . USER_TABLE . " WHERE qr_code = :qr_code LIMIT 1";
-		$query = $this->db->connect()->prepare($sql);
-		$query->bindParam(":qr_code", $qr_code);
 		$query->execute();
 		return $query->fetch(PDO::FETCH_ASSOC) ?: null;
 	}
 
 	public function getByUsername($username)
 	{
-		$sql = "SELECT * FROM " . USER_TABLE . " WHERE username = :username LIMIT 1";
+		$sql = "SELECT * FROM user WHERE username = :username LIMIT 1";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":username", $username);
 		$query->execute();
 		return $query->fetch(PDO::FETCH_ASSOC) ?: null;
+	}
+
+	public function getAllByRole($user_role)
+	{
+		$sql = "SELECT * FROM user WHERE user_role = :user_role";
+		$query = $this->db->connect()->prepare($sql);
+		$query->bindParam(":user_role", $user_role);
+		$query->execute();
+		return $query->fetchAll(PDO::FETCH_ASSOC) ?: null;
 	}
 
 	public function getAll(
@@ -76,12 +103,14 @@ class User
 				d.department_name,
 				uu.first_name AS creator_first_name,
 				uu.middle_name AS creator_middle_name,
-				uu.last_name AS creator_last_name
+				uu.last_name AS creator_last_name,
+				qr.qr_string
 			FROM
-				" . USER_TABLE . " u
+				user u
 				LEFT JOIN user_role ur ON u.user_role = ur.id
 				LEFT JOIN department d ON u.department = d.id
-				LEFT JOIN " . USER_TABLE . " uu ON u.created_by = uu.id
+				LEFT JOIN qr_code qr ON u.id = qr.user_id
+				LEFT JOIN user uu ON u.created_by = uu.id
 			WHERE 1 = 1";
 
 		$params = [];
@@ -91,8 +120,7 @@ class User
 							OR u.user_number LIKE :search
 							OR ur.role_name LIKE :search
 							OR d.department_name LIKE :search
-							OR d.abbreviation LIKE :search
-			";
+							OR d.abbreviation LIKE :search";
 			$params[':search'] = "%$search%";
 		}
 		if ($fullname !== '') {
@@ -105,11 +133,11 @@ class User
 		}
 		if ($department_name !== '') {
 			$sql .= " AND d.department_name = :department_name";
-			$params[':department_name'] = $department_name;
+			$params[':department_name'] = "%$department_name%";
 		}
 		if ($abbreviation !== '') {
 			$sql .= " AND d.abbreviation = :abbreviation";
-			$params[':abbreviation'] = $abbreviation;
+			$params[':abbreviation'] = "%$abbreviation%";
 		}
 
 		$sql .= " ORDER BY u.id";
@@ -118,59 +146,73 @@ class User
 		return $query->fetchAll(PDO::FETCH_ASSOC);
 	}
 
-	public function exists($username)
+	public function usernameExists($username)
 	{
 		return !empty($this->getByUsername($username));
 	}
 
-	public function create($first_name, $last_name, $middle_name, $username, $hashed_password, $qr_code, $user_role, $department, $created_by)
+	public function usernameExistsExceptCurrent($id, $username)
 	{
-		$sql = "INSERT INTO " . USER_TABLE . " (first_name, last_name, middle_name, username, hashed_password, qr_code, user_role, department, created_by)
-				  VALUES (:first_name, :last_name, :middle_name, :username, :hashed_password, :qr_code, :user_role, :department, :created_by)";
+		$sql = "SELECT 1 FROM user WHERE username = :username AND id != :id";
+		$query = $this->db->connect()->prepare($sql);
+		$query->bindParam(":id", $id, PDO::PARAM_INT);
+		$query->bindParam(":username", $username);
+		$query->execute();
+		return $query->fetchColumn() > 0;
+	}
+
+	public function create($first_name, $last_name, $middle_name, $username, $hashed_password, $user_role, $department, $created_by)
+	{
+		$sql = "INSERT INTO user (first_name, last_name, middle_name, username, hashed_password, user_role, department, created_by)
+				  VALUES (:first_name, :last_name, :middle_name, :username, :hashed_password, :user_role, :department, :created_by)";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":first_name", $first_name);
 		$query->bindParam(":last_name", $last_name);
 		$query->bindParam(":middle_name", $middle_name);
 		$query->bindParam(":username", $username);
 		$query->bindParam(":hashed_password", $hashed_password);
-		$query->bindParam(":qr_code", $qr_code);
 		$query->bindParam(":user_role", $user_role);
 		$query->bindParam(":department", $department);
 		$query->bindParam(":created_by", $created_by);
-		return $query->execute();
+		$query->execute();
+		return $this->db->connect()->lastInsertId();
 	}
 
-	public function update($id, $first_name, $middle_name, $last_name, $username, $user_role, $department, $created_at, $created_by)
+	public function update($id, $first_name, $middle_name, $last_name, $username, $user_role, $department, $created_at, $created_by = "", $hashed_password = "")
 	{
-		$sql = "UPDATE " . USER_TABLE . "
+		$sql = "UPDATE user
 				  SET first_name = :first_name, middle_name = :middle_name, last_name = :last_name,
-				  username = :username, user_role = :user_role, department = :department, created_at = :created_at, created_by = :created_by
-				  WHERE id = :id";
-		$query = $this->db->connect()->prepare($sql);
-		$query->bindParam(":id", $id);
-		$query->bindParam(":first_name", $first_name);
-		$query->bindParam(":middle_name", $middle_name);
-		$query->bindParam(":last_name", $last_name);
-		$query->bindParam(":username", $username);
-		$query->bindParam(":user_role", $user_role);
-		$query->bindParam(":department", $department);
-		$query->bindParam(":created_at", $created_at);
-		$query->bindParam(":created_by", $created_by);
-		return $query->execute();
-	}
+				  username = :username, user_role = :user_role, department = :department, created_at = :created_at";
 
-	public function updateCreator($id, $created_by)
-	{
-		$sql = "UPDATE " . USER_TABLE . " SET created_by = :created_by WHERE id = :id";
+		$params = [
+			":id" => $id,
+			":first_name" => $first_name,
+			":middle_name" => $middle_name,
+			":last_name" => $last_name,
+			":username" => $username,
+			":user_role" => $user_role,
+			":department" => $department,
+			":created_at" => $created_at,
+		];
+
+		if ($created_by !== '') {
+			$sql .= ", created_by = :created_by";
+			$params[":created_by"] = $created_by;
+		}
+		if ($hashed_password !== '') {
+			$sql .= ", hashed_password = :hashed_password";
+			$params[":hashed_password"] = $hashed_password;
+		}
+
+		$sql .= " WHERE id = :id";
+
 		$query = $this->db->connect()->prepare($sql);
-		$query->bindParam(":id", $id);
-		$query->bindParam(":created_by", $created_by);
-		return $query->execute();
+		return $query->execute($params);
 	}
 
 	public function delete($id)
 	{
-		$sql = "DELETE FROM " . USER_TABLE . " WHERE id = :id";
+		$sql = "DELETE FROM user WHERE id = :id";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":id", $id);
 		return $query->execute();
