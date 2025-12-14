8631925e37cb532be8cdc349d8986944c7e52f47
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-14 20:02:44 +0800

    ValidationService: another isValid function and minor changes

--- app/services/ValidationService.php
+++ app/services/ValidationService.php
@@ -5,16 +5,20 @@ use DateTime;
 
 class ValidationService
 {
-	public static function isValidTime($input)
+	public static function isValidTime($time) // HH:MM:SS
 	{
-		// HH:MM:SS
-		return preg_match("/^(2[0-3]|[01]?[0-9]):([0-5][0-9]):([0-5][0-9])$/", $input);
+		return preg_match("/^(2[0-3]|[01]?[0-9]):([0-5][0-9]):([0-5][0-9])$/", $time);
+	}
+
+	public static function isValidTimeNoSeconds($time) // HH:MM
+	{
+		return preg_match("/^(2[0-3]|[01]?[0-9]):([0-5][0-9])$/", $time);
 	}
 
 	public static function isIncompleteDateTime($input, $format = "Y-m-d H:i:s")
 	{
-		$datetime = \DateTime::createFromFormat($format, $input);
-		$errors = \DateTime::getLastErrors();
+		$datetime = DateTime::createFromFormat($format, $input);
+		$errors = DateTime::getLastErrors();
 
 		return (!$datetime || !empty($errors["warning_count"])) ? true : false;
 	}
