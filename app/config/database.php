c9e5e8379d87c35d0b1cd513f98ace2ece3c5cba
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-14 20:49:21 +0800

    db config: use environment variables for db credentials

--- app/config/database.php
+++ app/config/database.php
@@ -1,10 +1,12 @@
 <?php
-// Config
-const DB_HOST = "localhost";
-const DB_USER = "root";
-const DB_PASS = "";
-const DB_NAME = "theonary";
-const DB_PORT = 3306;
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define("DB_HOST", $_ENV["DB_HOST"]);
+define("DB_NAME", $_ENV["DB_NAME"]);
+define("DB_USER", $_ENV["DB_USER"]);
+define("DB_PASS", $_ENV["DB_PASS"]);
 
 // Table Names
 const DBT_DEPARTMENT = "department";
