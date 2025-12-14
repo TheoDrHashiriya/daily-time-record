c632e2458926c1986229b8ff926cede18a638ab1
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-14 19:52:25 +0800

    organized and added more relevant constants in config

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,18 +1,34 @@
 <?php
 const TIMEZONE = "Asia/Manila";
 
-/// DATABASE
-// Table Names
-const DEPARTMENT_TABLE = "department";
-const EVENT_TABLE = "event_record";
-const EVENT_TYPE_TABLE = "event_record_type";
-const SYSTEM_LOG_TABLE = "system_log";
-const USER_TABLE = "user";
-const USER_ROLE_TABLE = "user_role";
+/// TIME METRICS
+const EARLY_IN_OFFSET = -60 * 30;
+const EARLY_OUT_OFFSET = -60 * 15;
+const LATE_GRACE_PERIOD = 60 * 15;
 
 /// IDS / PRIMARY KEYS
+// System Log Types
+const LOG_AM_IN_EARLY = 1;
+const LOG_AM_IN = 2;
+const LOG_AM_IN_LATE = 3;
+const LOG_AM_IN_NULL = 4;
+const LOG_AM_OUT_EARLY = 5;
+const LOG_AM_OUT = 6;
+const LOG_AM_OUT_LATE = 7;
+const LOG_AM_OUT_NULL = 8;
+const LOG_PM_IN_EARLY = 9;
+const LOG_PM_IN = 10;
+const LOG_PM_IN_LATE = 11;
+const LOG_PM_IN_NULL = 12;
+const LOG_PM_OUT_EARLY = 13;
+const LOG_PM_OUT = 14;
+const LOG_PM_OUT_LATE = 15;
+const LOG_PM_OUT_NULL = 16;
+
 // User Roles
 const ROLE_ADMIN = 1;
+const ROLE_MANAGER = 2;
+const ROLE_EMPLOYEE = 3;
 
 // Record Types
 const AM_IN = 1;
--- app/config/database.php
+++ app/config/database.php
@@ -1,6 +1,17 @@
 <?php
+// Config
 const DB_HOST = "localhost";
 const DB_USER = "root";
 const DB_PASS = "";
 const DB_NAME = "theonary";
 const DB_PORT = 3306;
+
+// Table Names
+const DBT_DEPARTMENT = "department";
+const DBT_EVENT = "event_record";
+const DBT_EVENT_TYPE = "event_record_type";
+const DBT_NOTIFICATION_READ = "notification_read";
+const DBT_SYSTEM_LOG = "system_log";
+const DBT_SYSTEM_LOG_TYPE = "system_log_type";
+const DBT_USER = "user";
+const DBT_USER_ROLE = "user_role";
