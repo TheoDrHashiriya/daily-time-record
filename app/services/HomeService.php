18058723ca98b26d68c7d834e5faea6cec224021
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-14 20:10:56 +0800

    Home service: renames and alternative way of getting full names of record owners

--- app/services/HomeService.php
+++ app/services/HomeService.php
@@ -6,22 +6,27 @@ use App\Models\User;
 class HomeService
 {
 	private $authService;
-	private $erModel;
+	private $recordModel;
 
 	public function __construct()
 	{
 		$this->authService = new AuthService(new User);
-		$this->erModel = new EventRecord();
+		$this->recordModel = new EventRecord();
 	}
 
 	public function getAllData()
 	{
-		$rows = $this->erModel->getAll();
+		$rows = $this->recordModel->getAll();
 
 		foreach ($rows as &$record) {
 			$record["event_date_formatted"] = FormatService::formatDate($record["event_time"]);
 			$record["event_time_formatted"] = FormatService::formatTime($record["event_time"]);
 			$record["type_name_formatted"] = FormatService::formatEventType($record["type_name"]);
+			$record["full_name_formatted"] = FormatService::formatFullName(
+				$record["first_name"],
+				$record["middle_name"],
+				$record["last_name"],
+			);
 		}
 		unset($record);
 
