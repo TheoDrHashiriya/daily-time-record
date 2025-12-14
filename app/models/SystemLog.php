34d74a14cd866b66314965c6165a1a0894a3ecee
TheoDrHashiriya <theotrisha@gmail.com>
2025-12-14 20:05:15 +0800

    System Log model: get all log types, and mark as read function, as well as renaming and other minor refactors

--- app/models/SystemLog.php
+++ app/models/SystemLog.php
@@ -12,9 +12,17 @@ class SystemLog
 		$this->db = new Database();
 	}
 
+	public function getAllTypes()
+	{
+		$sql = "SELECT id, type_name, is_notification FROM system_log_type";
+		$query = $this->db->connect()->prepare($sql);
+		$query->execute();
+		return $query->fetchAll(PDO::FETCH_ASSOC);
+	}
+
 	public function getById($id)
 	{
-		$sql = "SELECT * FROM " . SYSTEM_LOG_TABLE . " WHERE id = :id LIMIT 1";
+		$sql = "SELECT * FROM system_log WHERE id = :id LIMIT 1";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":id", $id);
 		$query->execute();
@@ -23,38 +31,66 @@ class SystemLog
 
 	public function getAll()
 	{
-		$sql = "SELECT n.id, n.title, n.content, n.has_been_read, n.created_at, n.created_by,
+		$sql = "SELECT sl.id, sl.title, sl.content, sl.created_at, sl.created_by,
 			u.first_name, u.middle_name, u.last_name
-			FROM " . SYSTEM_LOG_TABLE . " n 
-			JOIN user u ON n.created_by = u.id
+			FROM system_log sl 
+			JOIN user u ON sl.created_by = u.id
 			ORDER BY created_at";
 		$query = $this->db->connect()->prepare($sql);
 		$query->execute();
 		return $query->fetchAll(PDO::FETCH_ASSOC);
 	}
 
-	public function getAllUnread()
-	{
-		$sql = "SELECT * FROM " . SYSTEM_LOG_TABLE . " WHERE has_been_read IS FALSE ORDER BY created_at";
+	public function markAllAsRead($user_id){
+		$sql = "
+				INSERT INTO notification_read (user_id, system_log, read_at)
+				SELECT :user_id, sl.id, NOW()
+				FROM system_log sl
+				JOIN system_log_type slt ON sl.system_log_type = slt.id
+				LEFT JOIN notification_read nr
+					ON nr.system_log = sl.id
+					AND nr.user_id = :user_id
+				WHERE slt.is_notification IS TRUE
+					AND nr.id IS NULL";
 		$query = $this->db->connect()->prepare($sql);
+		$query->bindParam(":user_id", $user_id);
+		return $query->execute();;
+	}
+
+	public function getAllUnread($user_id)
+	{
+		$sql = "
+				SELECT sl.*
+				FROM system_log sl
+				JOIN system_log_type slt
+					ON sl.system_log_type = slt.id
+				LEFT JOIN notification_read nr
+					ON nr.system_log = sl.id
+					AND nr.user_id = :user_id
+				WHERE slt.is_notification IS TRUE
+					AND nr.id IS NULL
+				ORDER BY sl.created_at DESC";
+		$query = $this->db->connect()->prepare($sql);
+		$query->bindParam(":user_id", $user_id);
 		$query->execute();
 		return $query->fetchAll(PDO::FETCH_ASSOC);
 	}
 
-	public function create($title, $content, $created_by)
+	public function create($title, $content, $created_by, $system_log_type)
 	{
-		$sql = "INSERT INTO " . SYSTEM_LOG_TABLE . " (title, content, created_by)
-				  VALUES (:title, :content, :created_by)";
+		$sql = "INSERT INTO system_log (title, content, created_by, system_log_type)
+				  VALUES (:title, :content, :created_by, :system_log_type)";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":title", $title);
 		$query->bindParam(":content", $content);
 		$query->bindParam(":created_by", $created_by);
+		$query->bindParam(":system_log_type", $system_log_type);
 		return $query->execute();
 	}
 
 	public function update($id, $title, $content, $created_by)
 	{
-		$sql = "UPDATE " . SYSTEM_LOG_TABLE . "
+		$sql = "UPDATE system_log
 				  SET title = :title, content = :content, created_by = :created_by
 				  WHERE id = :id";
 		$query = $this->db->connect()->prepare($sql);
@@ -67,7 +103,7 @@ class SystemLog
 
 	public function delete($id)
 	{
-		$sql = "DELETE FROM " . SYSTEM_LOG_TABLE . " WHERE id = :id";
+		$sql = "DELETE FROM system_log WHERE id = :id";
 		$query = $this->db->connect()->prepare($sql);
 		$query->bindParam(":id", $id);
 		return $query->execute();
